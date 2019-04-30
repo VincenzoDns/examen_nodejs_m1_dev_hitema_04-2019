2b90c5b298a6010626bfc3f5adf46551c3549298
Denis <vincenzoodns@gmail.com>
2019-04-30 17:04:24 +0200

    First

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,9 +1,18 @@
 const crypto = require('crypto');
 
 function sha1Encode(data) {
-    // To be implemented!
+  var hash = crypto.createHash('sha1');
+  hash.update(data);
+  return hash.digest('hex');
 }
 
 module.exports.digestAuth = (request, response, next) => {
-    // To be implemented!
+  const authorization = request.headers.authorization;  // 'Basic xxxx'
+  console.log('authorization ', authorization);
+  const encoded = authorization.replace('Basic ', '');
+  const decoded = Buffer.from(encoded, 'base64').toString('utf8');
+
+  const [login, password] = decoded.split(':');
+  if (login === 'node' && password === sha1Encode('password')) return next();
+  response.sendStatus(401);
 }
